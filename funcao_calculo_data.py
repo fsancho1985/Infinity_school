b272ae16b965d7cec9d0733ea1b13b8960fb7dfc
Francisco <fr_sancho@icloud.com>
2021-06-01 18:23:47 -0300

    adicionando comentarios ao desafio

--- funcao_calculo_data.py
+++ funcao_calculo_data.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
+# Variável que identifica a data atual
 data2 = datetime.now()
 
+# Função para cálculo da data
 def calculoData(data1, data2):
     data1 = datetime.strptime(data1, "%d/%m/%Y")
     return abs((data2 - data1).days)
