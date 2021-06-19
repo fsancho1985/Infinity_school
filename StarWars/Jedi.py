12048dd5b748202db57305e5f0b89da3d3469332
Francisco de Souza Sancho <77301081+fsancho1985@users.noreply.github.com>
2021-06-19 11:30:19 -0300

    Add files via upload
    Atualizando exercício aula online 18-06-2021

--- StarWars/Jedi.py
+++ StarWars/Jedi.py
@@ -1,7 +1,24 @@
 class Jedi:
-    def __init__(self, nome, grau, especie, sabre, nave = False):
+    def __init__(self, nome, especie, sabre, nomeDaNave = "", grau = "Padawan"):
         self.nome = nome
-        self.grau = grau
+        self.__grau = grau
         self.especie = especie
         self.sabre = sabre
-        self.nave = nave
+        self.nomeDaNave = nomeDaNave
+
+    @property
+    def grau(self):
+        return self.__grau
+
+    @grau.setter
+    def grau(self, novoGrau):
+        if novoGrau == "Padawan":
+            self.__grau = novoGrau
+            return True
+        elif novoGrau == "Cavaleiro":
+            self.__grau = novoGrau
+        elif novoGrau == "Mestre":
+            self.__grau = novoGrau
+            return True
+        else:
+            return False
---
+++ StarWars/main.py
@@ -0,0 +1,18 @@
+from Nave import Nave
+from Resistencia import Resistencia
+from Jedi import Jedi
+
+# novaNave = Nave("X-Wing", "Toyota", "X-Wing", "Caça", 350)
+# novaNave.tripulacao = 200
+# print(novaNave.tripulacao)
+
+
+# jedi1 = Jedi("Luke", "Humano", "vermelho")
+# jedi1.grau = "Cavaleiro"
+# print(jedi1.grau)
+
+
+membro = Resistencia("Luke", "Humano", "Piloto")
+membro.cargo = "Mecânico"
+membro.cargo = "Jedi"
+print(membro.cargo)
