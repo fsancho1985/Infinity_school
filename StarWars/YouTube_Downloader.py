131415cb572d4fba55da3574d54b5e8e90cda46c
Sancho <frsancho1985@gmail.com>
2021-06-23 22:08:01 -0300

    Atualizando

---
+++ StarWars/YouTube_Downloader.py
@@ -0,0 +1,14 @@
+import webbrowser
+import time
+import pyautogui
+
+#link = input("Insira o link do Youtube: ")
+
+#url = link
+
+#url = url[:12]+'ss'+url[12:]
+#webbrowser.open(url)
+time.sleep(5)
+pyautogui.position(x=617, y=308)
+
+
