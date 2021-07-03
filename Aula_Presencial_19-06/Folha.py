51dd4066e1ff265db0cf4f01df1bfb60f0d19381
Francisco de Souza Sancho <77301081+fsancho1985@users.noreply.github.com>
2021-07-03 11:38:48 -0300

    Add files via upload
    Atualizando

--- Aula_Presencial_19-06/Folha.py
+++ Aula_Presencial_19-06/Folha.py
@@ -2,6 +2,21 @@ class Folha:
     def __init__(self, funcionarios):
         self.__funcionarios = funcionarios
 
+    # getters
+
+    @property
+    def funcionarios(self):
+        for f in self.__funcionarios:
+            print("Funcionário: ", f.nome, "Salário: ", str(f.salario))
+
+    # Setters
+
+    @funcionarios.setter
+    def funcionarios(self, novoFuncionario):
+         self.__funcionarios.append(novoFuncionario)
+
+    # Método
+
     def totalFolha(self):
         total = 0
         for f in self.__funcionarios:
--- Aula_Presencial_19-06/main.py
+++ Aula_Presencial_19-06/main.py
@@ -2,20 +2,16 @@ from Funcionario import Funcionario
 from Folha import Folha
 
 funcionario1 = Funcionario("Francisco", 5000, "0001", "Dev")
-print("Salário do funcionário: ", funcionario1.nome, " R$",funcionario1.salario)
-
+#print("Salário do funcionário: ", funcionario1.nome, " R$",funcionario1.salario)
 funcionario2 = Funcionario("João", 4000, "002", "Web-Designer")
-print("Salário do funcionário: ", funcionario2.nome, " R$",funcionario2.salario)
-
+#print("Salário do funcionário: ", funcionario2.nome, " R$",funcionario2.salario)
 funcionario3 = Funcionario("Paulo", 7000, "003", "Java-Dev")
-print("Salário do funcionário: ", funcionario3.nome, " R$",funcionario3.salario)
-
+#print("Salário do funcionário: ", funcionario3.nome, " R$",funcionario3.salario)
 #funcionario1.salario = 6100
 #print("Aumento realizado no salário do funcionário: ",funcionario1.nome," foi de:", "R$",funcionario1.salario)
 #funcionario2.salario = 4400
 #print("Aumento realizado no salário do funcionário: ", funcionario2.nome, " foi de:", "R$", funcionario2.salario)
-
 funcionarios = [funcionario1, funcionario2, funcionario3]
 folha = Folha(funcionarios)
-
+print(folha.funcionarios)
 print("O total da folha é: R$",folha.totalFolha())
